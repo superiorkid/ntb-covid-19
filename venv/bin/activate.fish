5201b2e80f4054b7c0f517310c478a2add54ae1a
superiorkid <bayiPetani@protonmail.com>
2022-02-03 21:03:06 +0800

    install python pip and anothre python module

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/superiorkid/Project/indonesia-covid-tracker/venv"
+set -gx VIRTUAL_ENV "/home/superiorkid/Project/ntb-covid-information/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
